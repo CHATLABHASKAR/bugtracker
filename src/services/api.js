b1de47f0680e8729d577ffe2831d33365d5d26bf
CHATLABHASKAR <121053525+CHATLABHASKAR@users.noreply.github.com>
2026-01-05 14:45:36 +0530

    Change API base URL to production endpoint

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const client = axios.create({
-    baseURL: 'http://localhost:4000',
+    baseURL: 'https://bugtrackerbackend-2iui.onrender.com',
 });
 
 // Helper to validate MongoDB Object IDs
